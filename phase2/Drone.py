e42567ba27f6115d25afaae6a5c5408efc07200f
Surya prakashlokula <59243908+suryadevelope@users.noreply.github.com>
2024-05-15 21:46:52 +0530

    Create Drone.py

---
+++ phase2/Drone.py
@@ -0,0 +1,293 @@
+# 2=> Drone code
+
+#7/5/24 D1 drone relaying
+from pymavlink import mavutil
+import time
+
+from dronekit import connect,VehicleMode
+import logging
+import threading
+
+
+# Set up the logging configuration
+logging.basicConfig(filename='mission.log', filemode='w+', level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+logcount = 0
+
+def log_example(type,msg):
+    global logcount
+    if(logcount>20):
+        if(type == "D"):
+            logging.debug(msg)
+        elif(type == "I"):
+            logging.info(msg)
+        elif(type == "W"):
+            logging.warning(msg)
+        elif(type == "E"):
+            logging.error(msg)
+        logcount = 0
+    else:
+        logcount = logcount+1
+
+log_example("D","Initializing...")
+connection_string = "/dev/serial/by-id/usb-CubePilot_CubeOrange+_1F0047000B51323031393637-if00"#args.connect
+# Connect to the Vehicle
+print('Connecting to vehicle on: %s' % connection_string)
+vehicle = connect(connection_string, wait_ready=True, baud=921600)
+vehicle.wait_ready(True, raise_exception=False)
+latt = "17.418888"
+long ="78.654836"
+vehicle.airspeed = 5
+vehicle.groundspeed = 50
+vehicle.parameters['LAND_SPEED'] = 25 ##Descent speed of 30cm/s
+vehicle.parameters["WPNAV_SPEED"]=100
+log_example("D","Connecting to device")
+
+vehicle.mode = VehicleMode("STABILIZE")
+vehicle.armed = False
+log_example("W","Mode changed to STABILIZE")
+
+print(vehicle.version)
+
+# Replace 'serial_port' and 'baudrate' with your actual values
+# For example, '/dev/ttyUSB0' for Linux or 'COM3' for Windows
+serial_port = '/dev/ttyUSB0'  # Change this to your serial port
+baudrate = 115200  # Change this to your baud rate
+
+# Create a MAVLink connection
+mav = mavutil.mavlink_connection(serial_port, baud=baudrate)
+
+systemid = 101
+targetAltitude = 1.5 #meters
+
+print(mav)
+if(mav):
+    log_example("E","Telemetry not found")
+
+
+def landvehicle():
+        global rawarmedstate,relayingrawarmedstate
+        vehicle.mode = VehicleMode("LAND")
+        while True:
+            print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+            # Break and return from function just below target altitude.
+            if vehicle.location.global_relative_frame.alt <=0.74:
+                print("Reached ground")
+                time.sleep(1)
+                break
+            time.sleep(1)
+
+        while True:
+            vehicle.armed = False
+                # Confirm vehicle armed before attempting to take off
+            while vehicle.armed:
+                print(" Waiting for disarming...")
+                vehicle.armed = False
+                vehicle.mode = VehicleMode("STABILIZE")
+                time.sleep(1)
+            
+            break
+        rawarmedstate = False
+        relayingrawarmedstate = False
+
+def arm_takeoff(aTargetAltitude):
+        print("Arming motors")
+        # Copter should arm in GUIDED mode
+        vehicle.mode = VehicleMode("GUIDED")
+        vehicle.armed = True
+
+        # Confirm vehicle armed before attempting to take off
+        while not vehicle.armed:
+            print(" Waiting for arming...")
+            time.sleep(1)
+
+        print("Taking off!")
+        vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+        
+        while True:
+            print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+            # Break and return from function just below target altitude.
+            if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+                print("Reached target altitude")
+                break
+            time.sleep(1)
+        time.sleep(3)
+        landvehicle()
+        
+        
+
+count = 0
+
+
+rawarmedstate = False
+relayingrawarmedstate = False
+
+timestampcounter = 0
+
+relayingsendid,relayingsendtargetid,relayingsendcomid,relayingsendconfirm,relayingsendparam1,relayingsendparam2,relayingsendparam3,relayingsendparam4,relayingsendparam5,relayingsendparam6,relayingsendparam7 = 101,0,0,0,0,0,0,0,0,0,0
+# Function to send a MAVLink message
+def send_mavlink_message():
+    global timestampcounter,relayingsendid,relayingsendtargetid,relayingsendcomid,relayingsendconfirm,relayingsendparam1,relayingsendparam2,relayingsendparam3,relayingsendparam4,relayingsendparam5,relayingsendparam6,relayingsendparam7
+
+    vmode = 0
+
+    if(str(vehicle.mode).split(":")[1] == "STABLIZE"):
+        vmode=0
+    elif(str(vehicle.mode).split(":")[1] == "LOITER"):
+        vmode=1
+    elif(str(vehicle.mode).split(":")[1] == "RTL"):
+        vmode=2
+    elif(str(vehicle.mode).split(":")[1] == "GUIDED"):
+        vmode=3
+    elif(str(vehicle.mode).split(":")[1] == "AUTO"):
+        vmode=4
+
+   # Pack and send the custom message
+    msg = mav.mav.command_long_encode(
+        systemid, 100,  # System ID, trget ID
+        0,  # Command ID
+        0,  # Confirmation
+        vehicle._last_heartbeat, 
+        (-1 if vehicle.battery.level==None else vehicle.battery.level), 
+        vmode, 
+        (1 if(vehicle.armed==True) else 0),
+        timestampcounter, 0, 0  # Parameters 1-6
+    )
+    timestampcounter+=1
+    mav.mav.send(msg,force_mavlink1=True)
+    log_example("D",F"sent {vehicle._last_heartbeat},{vehicle.battery.level}, {vmode} , {vehicle.armed}")
+
+    msg1 = mav.mav.command_long_encode(
+        systemid, 100,  # System ID, Component ID
+        1,  # Command ID
+        0,  # Confirmation
+        vehicle.location.global_relative_frame.alt, 
+        vehicle.location.global_relative_frame.lat, 
+        vehicle.location.global_relative_frame.lon, 
+        vehicle.heading, 
+        vehicle.gps_0.satellites_visible, 
+        0, 0  # Parameters 1-6
+    )
+    log_example("D",F"sent {vehicle.location.global_relative_frame.alt},{vehicle.location.global_relative_frame.lat}, {vehicle.location.global_relative_frame.lon} , {vehicle.heading}, {vehicle.gps_0.satellites_visible}")
+
+    mav.mav.send(msg1,force_mavlink1=True)
+
+    if(int(relayingsendcomid) == 55):
+        print("relaying command received")
+        # print(relayingsendcomid)
+        # print(relayingsendparam1)
+        log_example("W","Relayed from "+str(relayingsendid)+" to "+str(relayingsendtargetid)+" for arm state "+str(relayingsendparam2)+" via "+str(systemid))
+        relayingsendmsg = mav.mav.command_long_encode(systemid,relayingsendtargetid,relayingsendcomid,relayingsendconfirm,relayingsendparam1,relayingsendparam2,relayingsendparam3,relayingsendparam4,relayingsendparam5,relayingsendparam6,relayingsendparam7)
+    else:
+        log_example("W",F"Timestamp comd={relayingsendcomid} from {relayingsendid} to {relayingsendtargetid} for arm state {relayingsendparam1} via {systemid}")
+        relayingsendmsg = mav.mav.command_long_encode(systemid,relayingsendtargetid,relayingsendcomid,relayingsendconfirm,relayingsendparam1,relayingsendparam2,relayingsendparam3,relayingsendparam4,relayingsendparam5,relayingsendparam6,relayingsendparam7)
+
+    mav.mav.send(relayingsendmsg,force_mavlink1=True)
+    
+    # print(vehicle._last_heartbeat)
+
+# Function to send a MAVLink message
+def resend_mavlink_message(id,targetid,comid,confirm,param1,param2,param3,param4,param5,param6,param7):
+    global relayingsendid,relayingsendtargetid,relayingsendcomid,relayingsendconfirm,relayingsendparam1,relayingsendparam2,relayingsendparam3,relayingsendparam4,relayingsendparam5,relayingsendparam6,relayingsendparam7
+
+    relayingsendid =id
+    relayingsendtargetid=targetid
+    relayingsendcomid = comid
+    relayingsendconfirm=confirm
+    relayingsendparam1=param1
+    relayingsendparam2=param2
+    relayingsendparam3=param3
+    relayingsendparam4=param4
+    relayingsendparam5=param5
+    relayingsendparam6=param6
+    relayingsendparam7=param7
+
+timestamp = 0
+# Function to receive and decode custom messages
+def receive_custom_messages():
+    global timestamp,rawarmedstate,relayingrawarmedstate
+    while True:
+        msg = mav.recv_match()  # Receive a message
+        if msg:
+            # print(msg)  # Print the raw message (for debugging)
+            # Decode the message to human-readable format
+
+            decoded_msg = msg.to_dict()
+            if(decoded_msg['mavpackettype'] == "COMMAND_LONG"):
+                log_example("W",F"Received data from {decoded_msg['target_system']}")
+                if(decoded_msg["target_component"]==systemid):
+                    
+                    # print(decoded_msg)
+                    if(decoded_msg["command"] == 200):
+                        timestamp = int(decoded_msg["param1"])
+                    elif(decoded_msg["command"] == 55):
+                        if(int(decoded_msg["param1"]) != systemid):
+                            
+                            resend_mavlink_message(decoded_msg["target_system"], int(decoded_msg["param1"]),decoded_msg["command"],0,decoded_msg["param1"], decoded_msg["param2"], decoded_msg["param3"], decoded_msg["param4"], decoded_msg["param5"], decoded_msg["param6"], decoded_msg["param7"])
+
+                        else:
+                            if(int(decoded_msg["param2"]) == 1):
+                                if(relayingrawarmedstate == False):
+                                    log_example("D","Relayed Drone Armed")
+                                    print("Relayed arm drone")
+                                    arm_takeoff(targetAltitude)
+
+                                relayingrawarmedstate = True
+
+                            else:
+                                if(relayingrawarmedstate == True):
+                                    log_example("D","Relayed Drone DisArmed")
+                                    print("Relayed disarm drone")
+                                    landvehicle()
+
+
+                                relayingrawarmedstate = False
+
+                    elif(decoded_msg["command"] == 215):
+                        if(int(decoded_msg["param1"]) == 1):
+                            if(rawarmedstate == False):
+                                log_example("D","Drone Armed")
+                                print("arm drone")
+                                arm_takeoff(targetAltitude)
+
+
+                            rawarmedstate = True
+
+                        else:
+                            if(rawarmedstate == True):
+                                log_example("D","Drone DisArmed")
+                                print("disarm drone")
+                                landvehicle()
+
+                            rawarmedstate = False
+                    
+                # else:
+                #     resend_mavlink_message(decoded_msg["target_system"], int(decoded_msg["target_system"]),decoded_msg["command"],0,decoded_msg["param1"], decoded_msg["param2"], decoded_msg["param3"], decoded_msg["param4"], decoded_msg["param5"], decoded_msg["param6"], decoded_msg["param7"])
+  
+               
+                    
+                # send_mavlink_message() 
+                continue
+                            # time.sleep(1)
+        
+        # send_mavlink_message()     
+        # time.sleep(0.2)            
+            
+        #Send a MAVLink message
+        # time.sleep(1)
+
+# Usage example
+
+
+def sendmessages():
+    while True:
+        send_mavlink_message()
+        time.sleep(0.3)
+
+thread = threading.Thread(target=sendmessages)
+thread.start()
+
+receive_custom_messages()
+print("program ended")
+
